1da6a8a5ced1a4266cd42423697566e67737cbaf
lschmidtfellner <127900788+lschmidtfellner@users.noreply.github.com>
2023-07-17 14:45:34 -0400

    added styling w/ darkmode classes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import "../styles/App.css";
+import Carousel from "./Carousel";
 
 function App() {
   return (
     <div className="App">
-      <h1>APP!</h1>
+      <Carousel/>
     </div>
   );
 }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,50 +1,44 @@
 import React from 'react'
+import redImage from '../assets/carousel_imgs/vino-red-wine-glass.jpeg'
+import whiteImage from '../assets/carousel_imgs/kira-white-wine-glass.jpeg'
+import roseImage from '../assets/carousel_imgs/Rose Glass For Horizontal Scroll.png'
+import orangeImage from '../assets/carousel_imgs/GettyImages-1171685839-e1682535087146.jpeg'
+import dessertImage from '../assets/carousel_imgs/riedel-sommeliers-sauternes_10.jpeg'
+import sparklingImage from '../assets/carousel_imgs/PorshaSmokeChmpgnFluteROF21.jpeg'
+
+import '../styles/carousel.css'
 
 function Carousel() {
   return (
-    <div className="carousel-container">
-      <div className="carousel-entry" id='carousel-red'>
-        <img
-          src="replace"
-          alt="Red Wine"
-        />
-        <h3 className="carousel-title">Red</h3>
+    <>
+      <h2>Varieties</h2>
+      <div className="carousel-container">
+        <div className="carousel-entry" id="carousel-red">
+          <img className="carousel-img" src={redImage} alt="Red Wine" />
+          <h3 className="carousel-title">Red</h3>
+        </div>
+        <div className="carousel-entry" id="carousel-white">
+          <img className="carousel-img" src={whiteImage} alt="White Wine" />
+          <h3 className="carousel-title">White</h3>
+        </div>
+        <div className="carousel-entry" id="carousel-rose">
+          <img className="carousel-img" src={roseImage} alt="Rosé" />
+          <h3 className="carousel-title">Rosé</h3>
+        </div>
+        <div className="carousel-entry" id="carousel-orange">
+          <img className="carousel-img" src={orangeImage} alt="Orange Wine" />
+          <h3 className="carousel-title">Orange</h3>
+        </div>
+        <div className="carousel-entry" id="carousel-sparkling">
+          <img className="carousel-img" src={sparklingImage} alt="Sparkling" />
+          <h3 className="carousel-title">Sparkling</h3>
+        </div>
+        <div className="carousel-entry" id="carousel-dessert">
+          <img className="carousel-img" src={dessertImage} alt="Dessert Wine" />
+          <h3 className="carousel-title">Dessert</h3>
+        </div>
       </div>
-      <div className="carousel-entry" id='carousel-white'>
-        <img
-          src="winebar/src/assets/carousel_imgs/kira-white-wine-glass.jpeg"
-        />
-        <h3 className="carousel-title">White</h3>
-      </div>
-      <div className="carousel-entry" id='carousel-rose'>
-        <img
-          src="winebar/src/assets/carousel_imgs/Rose Glass For Horizontal Scroll.png"
-          alt="Rosé"
-        />
-        <h3 className="carousel-title">Rosé</h3>
-      </div>
-      <div className="carousel-entry" id='carousel-orange'>
-        <img
-          src="winebar/src/assets/carousel_imgs/GettyImages-1171685839-e1682535087146.jpeg"
-          alt="Orange Wine"
-        />
-        <h3 className="carousel-title">Orange</h3>
-      </div>
-      <div className="carousel-entry" id='carousel-sparkling'>
-        <img
-          src="winebar/src/assets/carousel_imgs/PorshaSmokeChmpgnFluteROF21.jpeg"
-          alt="Sparkling"
-        />
-        <h3 className="carousel-title">Sparkling</h3>
-      </div>
-      <div className="carousel-entry" id='carousel-desert'>
-        <img
-          src="winebar/src/assets/carousel_imgs/riedel-sommeliers-sauternes_10.jpeg"
-          alt="Desert Wine"
-        />
-        <h3 className="carousel-title">Desert</h3>
-      </div>
-    </div>
+    </>
   )
 }
 
