7c15659a6f6bdabc608765e4c874c22570b9c750
lschmidtfellner <127900788+lschmidtfellner@users.noreply.github.com>
2023-07-18 15:24:59 -0400

    changed wine names to non duplicates

--- src/components/TableCellCard.js
+++ src/components/TableCellCard.js
@@ -23,9 +23,9 @@ function TableCellCard() {
             <img src="assets/Tri-card-wine_2.png" className="mx-auto w-auto h-auto p-4" />
           </div>
           <div className="w-3/5 ml-2">
-            <p className="text-sm mt-3 amiko-bold">Terranoble Chardonnay</p>
-            <p className="text-xs amiko-semi mt-1">Chile, Central Valley 2021</p>
-            <p className="text-2xl mt-1 amiko-semi font-bold">$11.99</p>
+            <p className="text-sm mt-3 amiko-bold">Bacchus Pinot Noir</p>
+            <p className="text-xs amiko-semi mt-1">California, 2020</p>
+            <p className="text-2xl mt-1 amiko-semi font-bold">$12.99</p>
             <button className="rounded-full rose-bg text-white py-1 w-5/6 mt-1">+ADD</button>
           </div>
         </div>
@@ -34,9 +34,9 @@ function TableCellCard() {
             <img src="assets/Tri-card-wine_3 copy.png" className="mx-auto w-auto h-auto p-4" />
           </div>
           <div className="w-3/5 ml-2">
-            <p className="text-sm mt-3 amiko-bold">Terranoble Chardonnay</p>
-            <p className="text-xs amiko-semi mt-1">Chile, Central Valley 2021</p>
-            <p className="text-2xl mt-1 amiko-semi font-bold">$11.99</p>
+            <p className="text-sm mt-3 amiko-bold">Whispering Angel</p>
+            <p className="text-xs amiko-semi mt-1">Côtes de Provence, 2022</p>
+            <p className="text-2xl mt-1 amiko-semi font-bold">$22.99</p>
             <button className="rounded-full rose-bg text-white py-1 w-5/6 mt-1">+ADD</button>
           </div>
         </div>
