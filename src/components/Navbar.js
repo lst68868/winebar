76124f12473602c1b965da1001f64c917bc98aff
granth2023 <grantharris38@gmail.com>
2023-07-18 09:32:30 -0400

    logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import '../styles/Navbar.css';
-
+import logo from '../../src/assets/logo.png'
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Logo Here</div> 
-      <div className="titlecard">Grape Escape</div>
+       <img src={logo} alt="Logo" />
       <button className="toggle-button"></button>
     </nav>
   );
