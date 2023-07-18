535964e39f7b7a6979b93b3fafdc0737f93652d9
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-07-18 09:54:57 -0400

    cicely

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import React, { useContext } from "react";
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import LandingPage from '../pages/LandingPage';
-import Carousel from '../components/Carousel'
-import Slideshow from '../components/Slideshow'
-import Switch from '../components/Switch'
-import { ThemeContext } from '../context/ThemeContext'
-import { useContext } from 'react'
+import Navbar from "../components/Navbar";
+import LandingPage from "../pages/LandingPage";
+import Switch from "../components/Switch";
+import { ThemeContext } from "../context/ThemeContext";
 
 function App() {
-  const {theme} = useContext(ThemeContext)
+  const { theme } = useContext(ThemeContext);
   return (
     <>
-    <Navbar />
-    <Routes>
-    <Route path="/" element={<LandingPage />} />
-    </Routes>
-    <Footer />
-    <div className={`App ${theme}`}>
-      <Slideshow className="w-10 h-10"/>
-      <Carousel />
+      <Navbar />
       <Switch />
-    </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+      <div className={`App ${theme}`}></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
